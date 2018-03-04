7b089e33a249a71c22f18110c19a9c75e63dc57d
Bernd K <prof7bit@gmail.com>
2018-03-04 20:15:51 +0100

    wait for network, add spmc 17

--- app/src/main/java/com/gmail/prof7bit/kodilauncher/MainActivity.java
+++ app/src/main/java/com/gmail/prof7bit/kodilauncher/MainActivity.java
@@ -1,24 +1,46 @@
 package com.gmail.prof7bit.kodilauncher;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.DataOutputStream;
 
+
 public class MainActivity extends Activity {
 
     private static final String TAG = "KODILauncher";
+    private static final int WAIT_TIME = 15;
+    private int counter;
+    private SharedPreferences settings;
+    private Toast toast;
+    private Handler handler = new Handler();
 
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void toast_show(String text) {
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
     private void launch() {
         // Load preferences
-        SharedPreferences settings;
         settings = PreferenceManager.getDefaultSharedPreferences(this);
 
         if (settings.getBoolean("fix_permissions", true)) {
@@ -35,7 +57,7 @@ public class MainActivity extends Activity {
 
             } catch (Exception e) {
                 Log.e(TAG, "could not execute su");
-                Toast.makeText(getApplicationContext(), "Kodi launcher couldn't execute su, couldn't fix permissions, needs root!", Toast.LENGTH_LONG).show();
+                toast_show("Kodi launcher couldn't execute su, couldn't fix permissions, needs root!");
 
             } finally {
                 try {
@@ -49,17 +71,35 @@ public class MainActivity extends Activity {
             }
         }
 
-        String xbmcPackage = settings.getString("xbmc_variant", getString(R.string.xbmc_package_default));
+        counter = WAIT_TIME;
+        try_launch();
+    }
 
-        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(xbmcPackage);
-        if (launchIntent != null) {
-            Log.d(TAG, "starting " + launchIntent.toString());
-            startActivity(launchIntent);
+    public void try_launch() {
+        if (isNetworkAvailable() || (counter == 0)) {
+            final String xbmcPackage = settings.getString("xbmc_variant", getString(R.string.xbmc_package_default));
+
+            Intent launchIntent = getPackageManager().getLaunchIntentForPackage(xbmcPackage);
+            if (launchIntent != null) {
+                Log.d(TAG, "starting " + launchIntent.toString());
+                startActivity(launchIntent);
+            } else {
+                Log.e(TAG, "could not find " + xbmcPackage + ", opening launcher settings instead");
+                Toast.makeText(getApplicationContext(), "Kodi launcher couldn't find " + xbmcPackage + ", please check the selected Kodi variant", Toast.LENGTH_SHORT).show();
+                Intent launchSettings = new Intent(this, LauncherSettingsActivity.class);
+                startActivity(launchSettings);
+            }
         } else {
-            Log.e(TAG, "could not find " + xbmcPackage + ", opening launcher settings instead");
-            Toast.makeText(getApplicationContext(), "Kodi launcher couldn't find " + xbmcPackage + ", please check the selected Kodi variant", Toast.LENGTH_LONG).show();
-            Intent launchSettings = new Intent(this, LauncherSettingsActivity.class);
-            startActivity(launchSettings);
+            if (counter < WAIT_TIME - 1) {
+                toast_show("waiting for network " + counter);
+            }
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    try_launch();
+                }
+            }, 1000);
+            counter -= 1;
         }
     }
 
